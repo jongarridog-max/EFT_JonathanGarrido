0a9257567e1bc1842de7dbb84dbccafeb7f23178
jongarridog-max <jon.garridog@duocuc.cl>
2026-07-13 21:08:25 -0400

    Update EFTjonathanGarrido.py

--- EFTjonathanGarrido.py
+++ EFTjonathanGarrido.py
@@ -70,14 +70,104 @@ def unidades_categoria(categoria,prendas_dict,bodega_dict,unidades_categoria):
     contador= 0
     while contador < len (tallas):
         cat=tallas[contador]
-        categoria_prendas ==prendas_dict [cat][1].lower()
-        if categoria_prendas == marca_buscada:
-            if cat in catalogo_dict:
-                tota_unidades = tota_unidades +catalogo_dict [cat][1]
+        unidades_categoria==prendas_dict [cat][1].lower()
+        if unidades_categoria == categoria_buscar:
+            if cat in bodega_dict:
+                tota_unidades = tota_unidades + prendas_dict[cat][1]
         contador = contador +1
     print("total unidades dsiponibles para la categoria",tota_unidades)
 
-def buscar_precio
+
+def busqueda_precio(p_min,p_max,prendas_dict,bodega_dict):
+    resultado=[]
+    tallas=list(bodega_dict.keys())
+    contador=0
+    while contador < len (tallas):
+        cat =tallas [contador]
+        precio=bodega_dict[cat][0]
+        unidades=bodega_dict[cat][1]
+        if precio >= p_min and precio <= p_max and unidades > 0:
+            if cat in prendas_dict:
+                caegoria= prendas_dict [cat][0]
+                resultado.append(caegoria + "--" + cat)
+        contador = contador +1
+    if len (resultado)==0:
+        print("No hay prendas en ese cango de precio")
+    else:
+        resultado.sort()
+        contador_res=0
+        while contador_res < len (resultado):
+            print(resultado[contador_res])
+            contador_res= contador_res +1
+
+def actualizar_precio_prenda(nuevo_precio,categoria,prendas_dict,bodega_dict):
+    if buscar_categoria(categoria,prendas_dict)== False:
+        return False
+    categoria_up=str(categoria).upper()
+    tallas=list(bodega_dict.keys())
+    contador =0
+    while contador < len (tallas):
+        k =tallas [contador]
+        if k.upper()== categoria_up:
+            bodega_dict [k][0]=int(nuevo_precio)
+            return True
+        contador = contador + 1
+    return False    
+
+def agregar_prenda(categoria,tallas,polera_basica,polera,jeans_slim,pantalon,chaqueta_urban,chaqueta,vestido_sol,vestido,poleron_cozt,poleron,camisa_formal,camisa,prendas_dict,bodega_dict):
+    categoria_up=str(categoria).upper()
+    if buscar_categoria(categoria_up,prendas_dict)== True:
+        return False
+    if str(pantalon).lower()=="s":
+        bool_pantalon= True
+    else:
+     bool_pantalon= False
+     prendas_dict[categoria_up]=[tallas,polera,polera_basica,int(chaqueta),bool_pantalon,chaqueta_urban,jeans_slim,vestido,vestido_sol,poleron,poleron_cozt,camisa,camisa_formal]
+     bodega_dict[categoria_up]=[int(precio),int(unidades)]
+    return True
+
+def eiminar_prenda(categoria,prenda_dict,bodega_dict):
+    if buscar_categoria(categoria,prenda_dict)== False:
+        categoria_up=str(categoria).upper()
+        tallas=list(prendas_dict.keys())
+        key_real=""
+        contador=0
+        while contador <len (tallas):
+            if tallas [contador].upper()== categoria_up:
+                key_real = tallas [contador]
+            contador = contador +1
+        if key_real !"":
+            prenda_dict.pop(key_real)
+            bodega_dict.pop(key_real)
+            return True
+        return False
+
+prendas = {
+    'S001': ['Polera Basica', 'polera', 'M', 'negro', 'algodon', True],
+    'S002': ['Jeans Slim', 'pantalon', 'L', 'azul', 'denim', False],
+    'S003': ['Chaqueta Urban', 'chaqueta', 'M', 'gris', 'poliester', True],
+    'S004': ['Vestido Sol', 'vestido', 'S', 'rojo', 'lino', False],
+    'S005': ['Poleron Cozy', 'poleron', 'XL', 'verde', 'algodon', True],
+    'S006': ['Camisa Formal', 'camisa', 'M', 'blanco', 'algodon', False],
+    ...
+}
+
+bodega = {
+    'S001': [7990, 12],
+    'S002': [19990, 0],
+    'S003': [29990, 3],
+    'S004': [24990, 6],
+    'S005': [17990, 8],
+    'S006': [14990, 2],
+    ...
+}
+
+
+
+
+
+
+
 
 
         
